cd729118968de5a41fdae1513bf96430b358db28
Bruno de Avila <brunomunao@outlook.com>
2023-03-09 15:39:53 -0300

    Exercícios diários.

---
+++ 1011/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            double pi = 3.14159;
+            double raio = double.Parse(Console.ReadLine());
+            Console.WriteLine($"VOLUME = {((4.0/3) * pi * Math.Pow(raio, 3)):F}");
+        }
+    }
+}
---
+++ 1012/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string entrada = Console.ReadLine();
+            string[] entradaToDouble = entrada.Split();
+            double A = double.Parse(entradaToDouble[0]);
+            double B = double.Parse(entradaToDouble[1]);
+            double C = double.Parse(entradaToDouble[2]);
+
+            double triangulo = A * C / 2;
+            double circulo = 3.14159 * (C * C);
+            double trapezio = (A + B) * C / 2;
+            double quadrado = B * B;
+            double retangulo = A * B;
+
+            Console.WriteLine($"TRIANGULO: {triangulo:F3}");
+            Console.WriteLine($"CIRCULO: {circulo:F3}");
+            Console.WriteLine($"TRAPEZIO: {trapezio:F3}");
+            Console.WriteLine($"QUADRADO: {quadrado:F3}");
+            Console.WriteLine($"RETANGULO: {retangulo:F3}");
+
+        }
+    }
+}
